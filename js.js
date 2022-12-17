94e9aac49d4361c325410c54f36a94c52774208d
osama7007 <saadosama43@gmail.com>
2022-12-17 20:34:51 +0200

    edit ui

--- js.js
+++ js.js
@@ -1,57 +1,39 @@
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissor = document.querySelector(".scissor");
-const winner = document.querySelector("span");
+
 const container = document.querySelector(".container");
+const winner = document.querySelector("span");
+function getValue(e) {
+	let btnValue = e.value;
+	return btnValue;
+}
 
 function result(e) {
 	let rand = Math.ceil(Math.random() * 3);
-	function getValue() {
-		let btnValue = e.value;
-		return btnValue;
-	}
-	if (rand == "1" && getValue() == "2") {
-		console.log("you win");
-	}
-
-	if (rand == "1" && getValue() == "2") {
-		container.innerHTML = ` <button style = "border : 3px solid red"><img  src="rock.png" ></button> <button style = "border : 3px solid blue "><img src="paper.jpg" ></button>`;
-		console.log("random : " + rand + "  /   you :" + getValue() + "  winner");
-
+	if (rand == "1" && getValue(e) == "2") {
+		container.innerHTML = ` <button style = "box-shadow: crimson 0 0 10px 1px"> <img  src="rock.jpg" ></button> <button style = "box-shadow: lightskyblue 0 0 10px 3px "><img src="papper.png" ></button>`;
 		winner.innerHTML = "you win";
-	} else if (rand == "1" && getValue() == "3") {
-		container.innerHTML = ` <button style = "border : 3px solid red"><img  src="rock.png" ></button> <button style = "border : 3px solid blue "><img src="scissor.png" ></button>`;
+	} else if (rand == "1" && getValue(e) == "3") {
+		container.innerHTML = ` <button style = "box-shadow: crimson 0 0 10px 1px"><img  src="rock.jpg" ></button> <button style = "box-shadow: lightskyblue 0 0 10px 3px "><img src="scissor.png" ></button>`;
 		winner.innerHTML = "you lose";
-		console.log("random : " + rand + "  /   you :" + getValue() + "   loser");
-	} else if (rand == "2" && getValue() == "1") {
-		container.innerHTML = ` <button style = "border : 3px solid red"><img  src="paper.jpg" ></button> <button style = "border : 3px solid blue "><img src="rock.png" ></button>`;
+	} else if (rand == "2" && getValue(e) == "1") {
+		container.innerHTML = ` <button style = "box-shadow: crimson 0 0 10px 1px"><img  src="papper.png" ></button> <button style = "box-shadow: lightskyblue 0 0 10px 3px "><img src="rock.jpg" ></button>`;
 		winner.innerHTML = "you lose";
-		console.log("random : " + rand + "  /   you :" + getValue() + "  loser");
-	} else if (rand == "2" && getValue() == "3") {
-		container.innerHTML = ` <button style = "border : 3px solid red"><img  src="paper.jpg" ></button> <button style = "border : 3px solid blue "><img src="scissor.png" ></button>`;
-		console.log("random : " + rand + "  /   you :" + getValue() + "  winner");
-
+	} else if (rand == "2" && getValue(e) == "3") {
+		container.innerHTML = ` <button style = "box-shadow: crimson 0 0 10px 1px"><img  src="papper.png" ></button> <button style = "box-shadow: lightskyblue 0 0 10px 3px "><img src="scissor.png" ></button>`;
 		winner.innerHTML = "you win";
-	} else if (rand == "3" && getValue() == "1") {
-		container.innerHTML = ` <button style = "border : 3px solid red"><img  src="scissor.png" ></button> <button style = "border : 3px solid blue "><img src="rock.png" ></button>`;
-		console.log("random : " + rand + "  /   you :" + getValue() + "  winner");
-
+	} else if (rand == "3" && getValue(e) == "1") {
+		container.innerHTML = ` <button style = "box-shadow: crimson 0 0 10px 1px"><img  src="scissor.png" ></button> <button style = "box-shadow: lightskyblue 0 0 10px 3px "><img src="rock.jpg" ></button>`;
 		winner.innerHTML = "you win";
-	} else if (rand == "3" && getValue() == "2") {
-		container.innerHTML = ` <button style = "border : 3px solid red"><img  src="scissor.png" ></button> <button style = "border : 3px solid blue "><img src="paper.jpg" ></button>`;
-		console.log("random : " + rand + "  /   you :" + getValue() + "  loser");
+	} else if (rand == "3" && getValue(e) == "2") {
+		container.innerHTML = ` <button style = "box-shadow: crimson 0 0 10px 1px"><img  src="scissor.png" ></button> <button style = "box-shadow: lightskyblue 0 0 10px 3px "><img src="papper.png" ></button>`;
 		winner.innerHTML = "you lose";
-	} else if (rand == "1" && getValue() == "1") {
-		console.log("random : " + rand + "  /   you :" + getValue() + "  tie");
-		container.innerHTML = ` <button style = "border : 3px solid red"><img  src="rock.png" ></button> <button style = "border : 3px solid blue "><img src="rock.png" ></button>`;
+	} else if (rand == "1" && getValue(e) == "1") {
+		container.innerHTML = ` <button style = "box-shadow: crimson 0 0 10px 1px"><img  src="rock.jpg" ></button> <button style = "box-shadow: lightskyblue 0 0 10px 3px "><img src="rock.jpg" ></button>`;
 		winner.innerHTML = "Tie";
-	} else if (rand == "2" && getValue() == "2") {
-		console.log("random : " + rand + "  /   you :" + getValue() + "  tie");
-		container.innerHTML = ` <button style = "border : 3px solid red"><img  src="paper.jpg" ></button> <button style = "border : 3px solid blue "><img src="paper.jpg" ></button>`;
+	} else if (rand == "2" && getValue(e) == "2") {
+		container.innerHTML = ` <button style = "box-shadow: crimson 0 0 10px 1px"><img  src="papper.png" ></button> <button style = "box-shadow: lightskyblue 0 0 10px 3px "><img src="papper.png" ></button>`;
 		winner.innerHTML = "Tie";
-	} else if (rand == "3" && getValue() == "3") {
-		console.log("random : " + rand + "  /   you :" + getValue() + "  tie");
-		container.innerHTML = ` <button style = "border : 3px solid red"><img  src="scissor.png"" ></button> <button style = "border : 3px solid blue "><img src="scissor.png"" ></button>`;
+	} else if (rand == "3" && getValue(e) == "3") {
+		container.innerHTML = ` <button style = "box-shadow: crimson 0 0 10px 1px"><img  src="scissor.png"" ></button> <button style = "box-shadow: lightskyblue 0 0 10px 3px "><img src="scissor.png"" ></button>`;
 		winner.innerHTML = "Tie";
 	}
 }
